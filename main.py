87b0049627ae5c2548297061cbbc2961bbdc4955
Yahya Qadeer Dar <yahyaqdar@gmail.com>
2024-05-05 17:02:33 +0500

    Convert String Data to Float From Dataset

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense
+from sklearn.preprocessing import LabelEncoder as lb
 
 from sklearn.model_selection import train_test_split
 
@@ -12,7 +13,8 @@ from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.callbacks import TensorBoard
 
 
-data = pd.read_csv('spam.csv', skiprows=1)
+data = pd.read_csv('spam.csv')
+
 
 # Data Preprocessing
 data['v2'] = data['v2'].str.lower()
@@ -21,12 +23,17 @@ data['v2'] = data['v2'].str.replace('[^a-zA-Z0-9]', '')
 lemmatizer = WordNetLemmatizer()
 data['v2'] = data['v2'].apply(lambda x: ' '.join([lemmatizer.lemmatize(word) for word in x.split()]))
 
+tonkenizer = Tokenizer(num_words=5000)
+tonkenizer.fit_on_texts(data['v2'])
+X_sequences = tonkenizer.texts_to_sequences(data['v2'])
+
+
 X = data['v2']
 y = data['v1']
 
-tonkenizer = Tokenizer(num_words=5000)
-tonkenizer.fit_on_texts(X)
-X_sequences = tonkenizer.texts_to_sequences(X)
+#Convert Categorial data(string) to Numerical Value(float)
+X = lb().fit_transform(X)
+y = lb().fit_transform(y)
 
 max_len = 100
 X_padded = pad_sequences(X_sequences, maxlen=max_len, padding='post')
